113fb2b1f029aa530096ca1479e68b280b726ffe
John Garrett <infantrygarrett@icloud.com>
2019-07-11 21:03:11 -0500

    wrapping up app.js

--- app.js
+++ app.js
@@ -22,3 +22,55 @@ app.engine('html', ejs.renderFile);
 app.use(express.static(__dirname + '/public'));
 
 // parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// index route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// catch form
+app.post('/', (req, res) => {
+    // res.send(req.body);
+    // console.log(req.body);
+    const { number, text } = req.body;
+
+    nexmo.message.sendSms(
+      '18474605091', number, text, {type: 'unicode' },
+      (err, responseData) => {
+          if(err) {
+              console.log(err);
+          } else {
+              const { messages } = responseData;
+              const { ['message-id']: id, ['to']: number, ['error-text']: error } = messages[0];
+              console.log(responseData);
+
+            //   get data
+            const data = {
+                id, 
+                number,
+                error
+            };
+
+            // emmit to client
+            io.emit('smsStatus', data);
+          }
+      }  
+    );
+});
+
+// port
+const port = 3000
+
+// starting server
+const server = app.listen(port, () => console.log('Server started on port ${port}'));
+
+// socket.io
+const io = socketio(server);
+io.on('connection', (socket) => {
+    console.log('Connected');
+    io.on('disconnect', () => {
+        console.log('Disconnected');
+    })
+});
