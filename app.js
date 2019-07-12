adf642772003448a83ec0ae6cdf193d44937209b
John Garrett <infantrygarrett@icloud.com>
2019-07-11 21:59:26 -0500

    changed port for heroku

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post('/', (req, res) => {
 });
 
 // port
-const port = 8080
+const port = process.env.PORT || 8080;
 
 // starting server
 const server = app.listen(port, () => console.log(`Server started on port ${port}`));
